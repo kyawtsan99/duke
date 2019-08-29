9693a720ec5067329a519529c594f21cecdd2894
kyawtsan99 <kyawtsan8899@gmail.com>
2019-08-29 22:45:52 +0800

    Level 2 - done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner; // Import the Scanner class
+import java.io.*;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,14 +15,23 @@ public class Duke {
 
         Scanner input = new Scanner(System.in); // Create a Scanner object
         boolean stringComparison = true;
+        int n = 0;
+        String[] arr = new String[100]; // Declaring the size of the string array to 100
 
         while (stringComparison) {
             String userInput = input.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 stringComparison = false;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < n; i++) {
+                    String x = arr[i];
+                    System.out.println( (i + 1) + ". " + x); // iterates through the entire array
+                }
             } else {
-                System.out.println(userInput);
+                arr[n] = userInput; // adds tasks into the array
+                System.out.println("added: " + userInput);
+                n++;
             }
         }
     }
