a7af581e86fc9ed9e632ae6682030401db15042c
kyawtsan99 <kyawtsan8899@gmail.com>
2019-08-29 22:00:37 +0800

    Level 1 - done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner; // Import the Scanner class
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+
+        Scanner input = new Scanner(System.in); // Create a Scanner object
+        boolean stringComparison = true;
+
+        while (stringComparison) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                stringComparison = false;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
