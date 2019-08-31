eefda455e94fc16c5e8d8041a38c8c933bccffcf
kyawtsan99 <kyawtsan8899@gmail.com>
2019-09-01 03:44:53 +0800

    Level 3 - done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,36 +2,57 @@ import java.sql.SQLOutput;
 import java.util.Scanner; // Import the Scanner class
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
 
         Scanner input = new Scanner(System.in); // Create a Scanner object
         boolean stringComparison = true;
-        int n = 0;
-        String[] arr = new String[100]; // Declaring the size of the string array to 100
+
+        ArrayList<Task> arr = new ArrayList<Task>(); // Creating array list
 
         while (stringComparison) {
             String userInput = input.nextLine();
+            // bye
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                stringComparison = false;
-            } else if (userInput.equals("list")) {
-                for (int i = 0; i < n; i++) {
-                    String x = arr[i];
-                    System.out.println( (i + 1) + ". " + x); // iterates through the entire array
+                stringComparison = false; // exits the loop
+            }
+            // list
+            else if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.print( (i + 1) + ". " ); // iterates through the entire array
+                    arr.get(i).listOutput();
                 }
-            } else {
-                arr[n] = userInput; // adds tasks into the array
-                System.out.println("added: " + userInput);
-                n++;
+            }
+            else {
+                String[] splitArr = userInput.split(" "); // splits the string by the white spaces
+                String firstWord = splitArr[0]; // obtains the first word of the string
+
+                // done
+                if (firstWord.equals("done")) {
+                    String secondWord = splitArr[1]; // obtains the number after done
+                    int num = Integer.parseInt(secondWord); // converts string into int
+                    arr.get(num - 1).taskIsDone();
+                    arr.get(num - 1).doneOutput();
+                }
+                // add
+                else {
+                    Task newTask = new Task(userInput);
+                    arr.add(newTask);
+                    //arr.add(userInput); // adds tasks into the array
+                    System.out.println("added: " + userInput);
+                }
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+
+public class Task {
+    protected String task;
+    protected boolean isDone;
+
+    public Task(String task) {
+        this.task = task;
+        this.isDone = false;
+    }
+
+    public void taskIsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); // return tick or X symbols
+    }
+
+    public void doneOutput() {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + this.getStatusIcon() + "] " + this.task);
+    }
+
+    public void listOutput() {
+        System.out.println("[" + this.getStatusIcon() + "] " + this.task);
+    }
+
+}
