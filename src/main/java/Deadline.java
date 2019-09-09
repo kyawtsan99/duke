cff7c430eb4214daa3eeb3ddb3f168df1eabb6ec
kyawtsan99 <kyawtsan8899@gmail.com>
2019-09-09 20:30:13 +0800

    Level-7 updated 3

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -4,13 +4,10 @@ import java.time.format.DateTimeFormatter;
 public class Deadline extends Task {
 
     protected String by;
-    //protected LocalDateTime formattedDueDate;
-    //protected static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
     public Deadline(String task, String by) {
         super(task);
         this.by = by;
-        //this.formattedDueDate = dueDateTime.parse(formatter);
     }
 
     @Override
